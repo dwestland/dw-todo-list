bd34f48a09c4f517a0cb95563c59a2d070140fef
Donald Westland <don@macbook-pro-1t.home>
2020-01-23 22:34:27 -0800

    Add completed functionality

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,14 +1,28 @@
 import React from "react";
+import PropTypes from 'prop-types'
+import './Todo.css'
 
 export default function Todo(props) {
-  const { task, id, completed, deleteTask } = props;
+  const { completed, completeTask, deleteTask, id, task } = props;
 
   return (
     <div>
       <li>
-        {task} {completed && <span>dun</span>}
+        <span className={completed ? "task": ""}>{task}</span>
+        <button onClick={() => completeTask(id)}>Completed</button>
         <button onClick={() => deleteTask(id)}>Delete</button>
       </li>
     </div>
   );
+}
+
+Todo.propTypes = {
+  completed: PropTypes.bool.isRequired,
+  completeTask: PropTypes.func.isRequired,
+  deleteTask: PropTypes.func.isRequired,
+  id: PropTypes.oneOfType([
+    PropTypes.string.isRequired,
+    PropTypes.number.isRequired
+  ]),
+  task: PropTypes.string.isRequired
 }
--- src/components/Todos.js
+++ src/components/Todos.js
@@ -4,8 +4,8 @@ import Todo from "./Todo";
 export default function Todos() {
   const initialState = [
     { id: 1, task: "Eat", completed: false },
-    { id: 2, task: "Sleep", completed: false },
-    { id: 3, task: "Code", completed: true }
+    { id: 2, task: "Sleep", completed: true },
+    { id: 3, task: "Code", completed: false }
   ];
   const [list, setList] = useState(initialState);
   console.log(list);
@@ -34,14 +34,36 @@ export default function Todos() {
     setNewTask("");
   };
 
+  const completeTask = (id) => {
+    const newList = list.map((item) => {
+      if (item.id === id) {
+        return item = {...item, completed: true}
+      }
+      return item
+    })
+      setList(newList)
+  };
+
+
+  // const completeTask = (id) => {
+  //   let newList = list.map((item) => {
+  //     if (item.id === id) {
+  //       return {}
+  //     }
+  //     setList(newList)
+  //   })
+  // };
+  // https://scotch.io/tutorials/4-uses-of-javascripts-arraymap-you-should-know
+
   return (
     <div>
-      <h1>Title</h1>
+      <h1>To-Do List</h1>
       <ul>
         <p>New task: {newTask}</p>
         {list.map(item => {
           return (
             <Todo
+              completeTask={completeTask}
               completed={item.completed}
               deleteTask={deleteTask}
               id={item.id}
